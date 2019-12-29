67b49c72f6534e39b6cf40b10e098af4a64c7d89
Richard <40587587+Quetives@users.noreply.github.com>
2019-12-29 14:06:18 -0800

    Step 63
    uploading the folder

--- Basic JavaScript Projects/JS/Basic_JavaScript_2.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -1,11 +1,17 @@
-function myFirstFunction() {
-    var str = "This Text is green!";
-    var result = str.fontcolor("green");
-    document.getElementById("Green_Text").innerHTML = result;
+function bluetext() {
+    var txt = "This Text is blue!";
+    var result = txt.fontcolor("blue");
+    document.getElementById("Blue_Text").innerHTML = result;
 }
 
 function myFunction() {
     var sentence = "I am learning";
-    Sentence += "a lot from this book!";
-    document.getElementById("concatenate").innerHTML = sentence;
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
+
+function Function_1() {
+    var saying = "This is a way to";
+    saying += " concatenate two phrases together!";
+    document.getElementById("string").innerHTML = saying;
 }
