9d07abe3fed4f0960a6276e9bbb18a344dfe8e61
Richard <40587587+Quetives@users.noreply.github.com>
2019-12-29 15:59:08 -0800

    Step 85
    Pushing the updated folder

--- Basic JavaScript Projects/JS/Basic_JavaScript_2.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -15,3 +15,15 @@ function Function_1() {
     saying += " concatenate two phrases together!";
     document.getElementById("string").innerHTML = saying;
 }
+
+function myFunction_1() {
+    var x = 50;
+    var y = 50;
+    var sum = x + y;
+    document.getElementById("function").innerHTML = sum;
+}
+
+function addition_function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
